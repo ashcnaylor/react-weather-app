5cc1694a2447d9d2f6844a9d363bca3577250f00
ashcnaylor <112088184+ashcnaylor@users.noreply.github.com>
2022-10-24 08:45:36 -0400

    added github url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <p>
+        <a href="https://github.com/ashcnaylor/react-weather-app">
+          This code is open source{" "}
+        </a>
+        by Ash Naylor
+      </p>
     </div>
   );
 }
